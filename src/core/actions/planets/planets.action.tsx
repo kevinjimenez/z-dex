c1d10f9e0da5e6d9624d84bf57e353199c19f558
kevzs <orlando.kj24@gmail.com>
2026-07-23 16:45:49 -0500

    ✨ Agregar acción e interfaz para obtener planetas desde la API

---
+++ src/core/actions/planets/planets.action.tsx
@@ -0,0 +1,22 @@
+import { dragonBallApi } from '@/core/api/dragon-ball.api';
+import { PlanetResponse } from '@/core/interfaces/responses/planet-response.interface';
+import { ApiResponse } from '@/interfaces/api-response.interface';
+
+export const planetsAction = async (page: number) => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    const { data } = await dragonBallApi.get<ApiResponse<PlanetResponse[]>>(
+      '/planets',
+      {
+        params: {
+          page,
+        },
+      },
+    );
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
