6aa8e76669816dfc7f153970971006ee52c6d99c
kevzs <orlando.kj24@gmail.com>
2026-07-23 17:22:14 -0500

    ♻️ Mover el loader de paginación fuera del ListFooterComponent para que no afecte el layout de la lista

--- src/app/(drawer)/(tabs)/planets/index.tsx
+++ src/app/(drawer)/(tabs)/planets/index.tsx
@@ -4,14 +4,15 @@ import LabelIcon from '@/shared/components/common/LabelIcon';
 import ScreenContainer from '@/shared/components/common/ScreenContainer';
 import BaseBadge from '@/shared/components/ui/BaseBadge';
 import { Image } from 'expo-image';
-import { DrawerToggleButton } from 'expo-router/drawer';
-import { FlatList, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import Animated, { SlideInDown, SlideOutDown } from 'react-native-reanimated';
 
 const PlanetsScreen = () => {
-  const { dragonBallPlanets } = usePlanets();
+  const { dragonBallPlanets, fetchNextPage, isLoading, isFetchingNextPage } =
+    usePlanets();
 
   return (
-    <ScreenContainer>
+    <ScreenContainer className="pb-10">
       <View className="mb-6 flex-row gap-x-4 items-center">
         <CustomDrawerMenuButton />
         <View className="flex-col gap-y-2">
@@ -21,42 +22,67 @@ const PlanetsScreen = () => {
           </Text>
         </View>
       </View>
-      <FlatList
-        data={dragonBallPlanets}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => String(item.id)}
-        ItemSeparatorComponent={() => <View className="h-4" />}
-        renderItem={({ item }) => (
-          <View className="bg-white rounded-xl overflow-hidden border border-secondary/20">
-            <Image
-              source={{ uri: item.image }}
-              contentPosition="center"
-              style={{ width: '100%', height: 140 }}
-            />
-            <View className="flex-col px-5 py-4 gap-y-4">
-              <View className="flex-row flex-1 justify-between items-center">
-                <LabelIcon
-                  text={item.name}
-                  prefixIcon="earth"
-                  customClassText="text-xl font-oswald-bold"
-                  customClassContainer="gap-x-3"
-                  size={20}
-                  color="text-primary"
-                />
-                <BaseBadge
-                  text={item.isDestroyed ? 'Destruido' : 'Activo'}
-                  customClassText="text-xs text-white font-dmsans-bold"
-                  customClassBadge="bg-primary"
-                />
-              </View>
-
-              <Text className="text-pretty font-dmsans-medium text-ink-3">
-                {item.description}
-              </Text>
-            </View>
+      {isLoading ? (
+        <View className="flex-1 items-center justify-center">
+          <View className="flex-col items-center justify-center gap-y-4">
+            <ActivityIndicator size={65} className="text-ink-3" />
+            <Text className="font-dmsans-semibold text-ink-3">Cargando...</Text>
           </View>
-        )}
-      />
+        </View>
+      ) : (
+        <View className="flex-1">
+          <FlatList
+            data={dragonBallPlanets}
+            showsVerticalScrollIndicator={false}
+            onEndReachedThreshold={0.6}
+            onEndReached={() => fetchNextPage()}
+            keyExtractor={(item) => String(item.id)}
+            ItemSeparatorComponent={() => <View className="h-4" />}
+            renderItem={({ item }) => (
+              <View className="bg-white rounded-xl overflow-hidden border border-secondary/20">
+                <Image
+                  source={{ uri: item.image }}
+                  contentPosition="center"
+                  style={{ width: '100%', height: 140 }}
+                />
+                <View className="flex-col px-5 py-4 gap-y-4">
+                  <View className="flex-row flex-1 justify-between items-center">
+                    <LabelIcon
+                      text={item.name}
+                      prefixIcon="earth"
+                      customClassText="text-xl font-oswald-bold"
+                      customClassContainer="gap-x-3"
+                      size={20}
+                      color="text-primary"
+                    />
+                    <BaseBadge
+                      text={item.isDestroyed ? 'Destruido' : 'Activo'}
+                      customClassText="text-xs text-white font-dmsans-bold"
+                      customClassBadge="bg-primary"
+                    />
+                  </View>
+
+                  <Text className="text-pretty font-dmsans-medium text-ink-3">
+                    {item.description}
+                  </Text>
+                </View>
+              </View>
+            )}
+          />
+
+          {isFetchingNextPage && (
+            <Animated.View
+              entering={SlideInDown.duration(250)}
+              exiting={SlideOutDown.duration(200)}
+              className="absolute bottom-4 left-0 right-0 items-center"
+            >
+              <View className="bg-frame p-2 rounded-full">
+                <ActivityIndicator className="text-ink-3" size={30} />
+              </View>
+            </Animated.View>
+          )}
+        </View>
+      )}
     </ScreenContainer>
   );
 };
