eacb9dbc4f6fd0125ddeb1bef7b85eb9ea5fcf61
kevzs <orlando.kj24@gmail.com>
2026-07-23 09:56:28 -0500

    🐛 Agregar delay artificial en characterByIdAction para probar el skeleton

--- src/core/actions/characters/character-by-id.action.ts
+++ src/core/actions/characters/character-by-id.action.ts
@@ -3,6 +3,8 @@ import { CharacterResponse } from '@/core/interfaces/responses/character-respons
 
 export const characterByIdAction = async (id: number) => {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     const { data } = await dragonBallApi.get<CharacterResponse>(
       `/characters/${id}`,
     );
